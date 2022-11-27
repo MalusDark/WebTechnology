c6032c7a396bce2fd63beb5e2874547662253181
MalusDarc <artem.kotegov@mail.ru>
2022-11-27 17:27:16 +0300

    CR одной таблицы

---
+++ LR6/CR_Table.php
@@ -0,0 +1,131 @@
+<?php
+    session_start();
+    
+    if(isset($_POST["add"])) 
+    {
+        $deliveryAdress=htmlspecialchars(($_POST["Addres"]));
+        $orderNote=htmlspecialchars(($_POST["Note"]));
+        $orderCost=doubleval(htmlspecialchars(($_POST["Cost"])));
+        $customerId=htmlspecialchars(($_POST["nameFF"]));
+                
+        if(isset($_FILES) && $_FILES['fileFF']['error'] == 0)
+        {
+            $uploaddir = 'C:\OpenServer\domains\localhost\LR6\image\\';
+            $uploadfile = $uploaddir . basename($_FILES['fileFF']['name']);
+            $tmp_name = $_FILES["fileFF"]["tmp_name"];
+            $name = basename($_FILES["fileFF"]["name"]);
+            move_uploaded_file($tmp_name, "$uploaddir/$name"); 
+            $invoiceScan=htmlspecialchars($_FILES["fileFF"]["name"]);
+            $message ='Файл успешно загружен';
+            UserTable::add($invoiceScan,$deliveryAdress,$orderNote,$orderCost,$customerId);
+        }
+        else
+        {
+            $message = 'Ошибка загрузки файла';
+        }
+    }
+    class UserTable 
+    { 
+        public static function open()
+        {
+            $hostname = "localhost";
+            $username = "root";
+            $password = "root";
+            $databaseName = "orders";
+            $connect = mysqli_connect($hostname, $username, $password, $databaseName);
+            $query = "SELECT transactions.invoiceId,transactions.invoiceScan,transactions.deliveryAdress,transactions.orderNote,transactions.orderCost,customers.customerName FROM transactions inner join customers ON transactions.customerId = customers.customerId ORDER BY transactions.invoiceId ASC";
+            $result_table = mysqli_query($connect, $query);
+            
+            while($row1 = mysqli_fetch_array($result_table))
+            {
+                $resualt_text = $resualt_text. "<tr><td>".$row1[0]."</td>
+                    <td><img src=\"image/".$row1[1]."\" height=250 width=480></img></td>
+                    <td>".$row1[2]."</td>
+                    <td>".$row1[3]."</td>
+                    <td>".$row1[4]."</td>
+                    <td>".$row1[5]."</td>
+                </tr>";
+            }
+            return $resualt_text;
+        }
+        public static function add(string $invoiceScan, string $deliveryAdress, string $orderNote, float $orderCost, int $customerId)
+        {
+            $hostname = "localhost";
+            $username = "root";
+            $password = "root";
+            $databaseName = "orders";
+            $connect = mysqli_connect($hostname, $username, $password, $databaseName);
+            
+            $query = "INSERT INTO `transactions`(invoiceScan, deliveryAdress, orderNote, orderCost, customerId) VALUES('$invoiceScan','$deliveryAdress','$orderNote','$orderCost','$customerId');";
+            
+            $update_table = mysqli_query($connect, $query);
+            if($update_table != FALSE)
+            {
+                $message="Запись успешно добавлена";
+            }
+            else
+            {
+                $message="Ошибка добавления записи";
+            }
+            
+            return $message;
+        }
+    } 
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WoodFamily</title>
+    <link rel="stylesheet" href="MainStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+    <?php include "header.php"; ?>
+    
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-striped table-dark">
+                <thead>
+                    <tr>
+                        <th style="width:20px">Id</th>
+                        <th style="width:500px">Скан</th>
+                        <th style="width:180px">Адрес</th>
+                        <th style="width:120px">Пояснение</th>
+                        <th style="width:80px">Цена</th>
+                        <th style="width:80px">Имя пользователя</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php echo $text=UserTable::open(); ?>
+                </tbody>
+            </table>
+            <form enctype="multipart/form-data" method="post" id="feedback-form">
+                <label for="fileFF">Прикрепить Скан:</label>
+                <br>
+                <input type="file" name="fileFF" multiple id="fileFF" style="width:280px">
+                <label for="Address">Адрес:</label>
+                <input type="text" name="Addres" id="Address" style="width:120px">
+                <label for="Note">Пояснение:</label>
+                <textarea name="Note" id="messageFF" required rows="5" placeholder="Детали заявки…" style="width:120px"></textarea>
+                <label for="Cost">Цена:</label>
+                <input type="number" name="Cost" id="Costt" style="width:120px">
+                <label for="nameFF">Айди пользователя:</label>
+                <input type="number" name="nameFF" id="nameFF" x-autocompletetype="name" style="width:120px">
+                <br>
+                <input name="add" value="Добавить" type="submit" id="submitFF">
+            </form>
+            <?php
+                echo $message;
+            ?>
+        </div>
+    </div>
+    
+    <?php include "footer.php"; ?>
+    
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
---
+++ LR6/header.php
@@ -0,0 +1,153 @@
+<div class="d-flex flex-column min-vh-100">
+        <div class="container-fluid" >
+            <div class="row" style="max-width: 45%">
+                <div class="col-3">
+                     <a class="contact-phone" href="tel:89375381690" title="8 800 555 35 35">
+                         <span> 8 937 538 16 90</span>
+                     </a>
+                </div>
+                <div class="col-3">
+                    <div class="work-time">
+                        <span>9:00 - 18:00</span>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <a class="contact-mail" href="mailto:artem.kotegov@mail.ru" title="artem.kotegov@mail.ru">
+                        <span>artem.kotegov@mail.ru</span>
+                    </a>
+                </div>
+            </div>
+            <div class="container-fluid" style="background-color: #F8F9FA">
+            <div class="row" style="box-shadow:
+                 0 15px 15px  -15px dimgrey ,
+                 0 -15px 15px  -15px dimgrey;">
+                <div class="col-12">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="#" onClick='location.href="index.php"'>
+                                <img src="image/icon.png" style="max-width: 250px;max-height: 150px;">
+                            </a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Переключатель навигации">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav me-auto">
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            О нас
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                            <li><a class="dropdown-item" href="#">JAF в мире</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Продукция из древесины
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                                            <ul class="navbar-nav">
+                                                <li class="nav-item dropdown">
+                                                    <a class="nav-link dropdown-toggle" href="#" role="button" id="navbarDropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        Пиломатериалы
+                                                    </a>
+                                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                                        <li><a class="dropdown-item" href="#">Обрезанная доска</a></li>
+                                                        <li><a class="dropdown-item" href="#">Необрезанная доска</a></li>
+                                                        <li><a class="dropdown-item" href="#">Слэбы</a></li>
+                                                        <li><a class="dropdown-item" href="#">Торцевые спилы и капы</a></li>
+                                                        <li><a class="dropdown-item" href="#">Столярная заготовка</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                            <li><a class="dropdown-item" href="#">Шпон</a></li>
+                                            <li><a class="dropdown-item" href="#">Террасная доска</a></li>
+                                            <li><a class="dropdown-item" href="#">Фасадная доска</a></li>
+                                            <li><a class="dropdown-item" href="#">Термососна</a></li>
+                                            <li><a class="dropdown-item" href="#">Термоясень</a></li>
+                                            <li><a class="dropdown-item" href="#">Масло для дерева</a></li>
+                                            <li><a class="dropdown-item" href="#">Термососна</a></li>
+                                            <li><a class="dropdown-item" href="#">Крепеж</a></li>
+                                            <li><a class="dropdown-item" href="#">Столярная плита</a></li>
+                                            <li><a class="dropdown-item" href="#">Мебельный щит</a></li>
+                                            <li><a class="dropdown-item" href="#">Панели Мдф</a></li>
+                                            <li><a class="dropdown-item" href="#">Паркет</a></li>
+                                            <ul class="navbar-nav">
+                                                <li class="nav-item dropdown">
+                                                    <a class="nav-link dropdown-toggle" href="#" role="button" id="navbarDropdownMenuLink1" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        Фанера
+                                                    </a>
+                                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink1">
+                                                        <li><a class="dropdown-item" href="#">Ламинированная фанера</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                            <li><a class="dropdown-item" href="#">Клей</a></li>
+                                            <li><a class="dropdown-item" href="#">Напольные покрытия Eurowood</a></li>
+                                            <li><a class="dropdown-item" href="#">Погонаж из лиственницы</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Кровельная продукция RUUKKI
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown3">
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Hyygge</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Finnera</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Frigge</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Adamante</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Elite</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Monterrey</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Monterrey FEB</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Classic C</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Classic D</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Classic Silence</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Classic Authentic</a></li>
+                                            <li><a class="dropdown-item" href="#">Металлочерепица Ruukki Classic M</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown4" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Заказ и оплата
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown4">
+                                            <li><a class="dropdown-item" href="#">Оплата на сайте</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-x" href="#" id="navbarDropdown5" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick='location.href="orders.php?minimum=0&maximum=999999&select_address=all&name=&note="'>
+                                            Доставка
+                                        </a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-x" href="#" id="navbarDropdown6" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick='location.href="CR_Table.php"'>
+                                            CR одной таблицы
+                                        </a>
+                                    </li>
+                                </ul>
+                                <form class="d-flex">
+                                    <input class="form-control me-2" type="search" placeholder="Поиск" aria-label="Поиск">
+                                    <button class="btn btn-outline-success" type="submit">Поиск</button>
+                                </form>
+                                <div class="registration">
+                                     <?php
+                                    if ($_SESSION['user_nem'] != "")
+                                    {
+                                        echo "Вы вошли как ". $_SESSION['user_nem'] . "<br>";
+                                        echo "<a href='index.php?out=true'>Выйти</a>";
+                                    }
+                                    else
+                                    {
+                                        echo "Вы не авторизованы.";
+                                        echo "<br>";
+                                        echo "<a href='register.php'>Зарегистрироваться</a>";
+                                        echo "<br>";
+                                        echo "<a href='login.php'>Войти</a>";
+                                    }
+                                ?>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
